2bb982aea46703f6e884c14b00fafe9e82ad010e
Lolfoollor <L_Z_F47@hotmail.com>
2024-07-16 18:42:55 +0800

    feat: Add footer to the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import Button from "./Button";
+import styles from "./Footer.module.css";
+// import { useNavigate } from "react-router-dom";
+
+
+const content = [
+  {name:"Help Center", link: "/faq"},
+  {name:"Contact Us", link: "/contact"},
+  {name:"Follow Us", link: "/follow"},
+  {name:"Jobs", link: "/jobs"},
+  {name:"Terms & Privacy", link: "/terms-and-privacy"}
+]
+
+function Footer() {
+  // const navigate = useNavigate();
+
+  const handleClick = (item) => {
+    // navigate(item.link);
+  };
+
+  return (
+    <div className={styles.footer}>
+      {content && content.map((item) => (
+          <div key={item.name}>
+            <Button onClick={() => handleClick(item.link)}
+                    label={item.name}
+            />
+          </div>
+        ))}
+    </div>
+  )
+}
+
+export default Footer;
