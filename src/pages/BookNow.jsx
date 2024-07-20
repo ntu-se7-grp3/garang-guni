a834d91982619277e2fc64e9d70cc591007095fa
Lolfoollor <L_Z_F47@hotmail.com>
2024-07-21 03:23:59 +0800

    Add basic template for bookNow Page

--- src/pages/BookNow.jsx
+++ src/pages/BookNow.jsx
@@ -1,4 +1,41 @@
+const HAIKU = ["Time for new chapters", "Treasure await new hands", "Book your slot today"];
+
+const splitLastWord = (str) => {
+  const wordArr = str.split(" ");
+  return [wordArr.slice(0, -1).join(" ") + " ", wordArr.slice(-1).toString()];
+};
+
 function BookNow() {
+
+  return (
+    <>
+      <div>
+        <div>
+          <p>Turn your trash into cash</p>
+          <div>
+            {HAIKU.map((text, i) => {
+              if (i === 1) {
+                const [preHighlightWord, highlightWord] = splitLastWord(text);
+                return <p key={i}>
+                  {preHighlightWord}<span className="highlight">{highlightWord}</span>
+                </p>
+              }
+              return <p key={i}>{text}</p>;
+            })}
+          </div>
+          <p>Find the list of eligible items for trade <button>here!</button></p>
+        </div>
+        <div>
+          <div style={{display:"flex"}}>
+            <div className="chooseLocation"></div>
+            <div className="chooseDate"></div>
+            <button></button>
+          </div>
+        </div>
+      </div>
+    </>
+
+  );
 
 }
 
