55e85326ae819410e92c5e3d9588e0e8a8759381
Lolfoollor <L_Z_F47@hotmail.com>
2024-07-22 17:35:20 +0800

    Add css into BookNow.jsx

--- src/pages/BookNow.jsx
+++ src/pages/BookNow.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import { FormControl, InputLabel, Select, MenuItem } from '@mui/material'
+import { Link } from 'react-router-dom'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { FormControl, Select, MenuItem } from "@mui/material";
+
 
 import styles from "./BookNow.module.css";
 import backEnd from "../api/back-end-api";
+import recyclingCircle from "../assets/recycleCircle.png";
 
-const HAIKU = ["Time for new chapters", "Treasure await new hands", "Book your slot today"];
+const HAIKU = ["Time for new chapters", "Treasure await new HANDS", "Book your slot today"];
 
 const splitLastWord = (str) => {
   const wordArr = str.split(" ");
@@ -14,15 +17,19 @@ const splitLastWord = (str) => {
 };
 
 /* This is temp until login and register component is done.*/
-const getFakeToken = () => {return "70d3af81-7c2d-49a1-b5c4-f82ae8979426";}
+const getFakeToken = () => {
+  return "70d3af81-7c2d-49a1-b5c4-f82ae8979426";
+};
 
-const isValidSession = (token) => {return token === "70d3af81-7c2d-49a1-b5c4-f82ae8979426";}
+const isValidSession = (token) => {
+  return token === "70d3af81-7c2d-49a1-b5c4-f82ae8979426";
+};
 
-const locationAreasInSg = ["Central","East","North","North-East","West"];
+const locationAreasInSg = ["Central", "East", "North", "North-East", "West"];
 
 function BookNow() {
   const [dateAndLocations, setDateAndLocations] = useState({});
-  const [listOfAvailDates, setlistOfAvailDates]= useState([]);
+  const [listOfAvailDates, setlistOfAvailDates] = useState([]);
   const [selectedDate, setSelectedDate] = useState(null);
   const [selectedLocation, setSelectedLocation] = useState("");
   const sessionToken = getFakeToken();
@@ -30,7 +37,7 @@ function BookNow() {
   useEffect(() => {
     console.log("Getting date and location");
     getDatesAndLocations();
-  },[]);
+  }, []);
 
   const getDatesAndLocations = async () => {
     try {
@@ -43,10 +50,11 @@ function BookNow() {
       setDateAndLocations(response.data[0]);
       console.log("✅ Data retrieved");
       console.log("⏳ Converting to list of available dates");
-      setlistOfAvailDates(Object.keys(response.data[0].dateWithLocation)
-                                 .map((unixStr) => {
-                                   return new Date(parseInt(unixStr)* 1000);
-                                 }));
+      setlistOfAvailDates(
+        Object.keys(response.data[0].dateWithLocation).map((unixStr) => {
+          return new Date(parseInt(unixStr) * 1000);
+        }),
+      );
       console.log("✅ Converted Successfully");
     } catch (error) {
       console.error(`🚨 Error: ${error.code}(${error.response.status}): ${error.message}`);
@@ -71,41 +79,52 @@ function BookNow() {
 
   return (
     <>
-      <div>
-        <div>
-          <p>Turn your trash into cash</p>
+      <div className={styles.landingPageBg}>
+        <div className={styles.textBoxContainer}>
           <div>
-            {HAIKU.map((text, i) => {
-              if (i === 1) {
-                const [preHighlightWord, highlightWord] = splitLastWord(text);
-                return <p key={i}>
-                  {preHighlightWord}<span className="highlight">{highlightWord}</span>
-                </p>
-              }
-              return <p key={i}>{text}</p>;
-            })}
+            <p className={`${styles.textFont} ${styles.haikuHeader}`}>Turn your trash into cash</p>
+            <div className={`${styles.textFont} ${styles.haiku}`}>
+              {HAIKU.map((text, i) => {
+                if (i === 1) {
+                  const [preHighlightWord, highlightWord] = splitLastWord(text);
+                  return (
+                    <p key={i}>
+                      {preHighlightWord}
+                      <span>{highlightWord}</span>
+                    </p>
+                  );
+                }
+                return <p key={i}>{text}</p>;
+              })}
+            </div>
+            <p className={styles.haikuFooter}>
+              Find the list of eligible items for trade <Link to='/list'>here!</Link>
+            </p>
           </div>
-          <p>Find the list of eligible items for trade <button>here!</button></p>
-        </div>
-        <div>
-          <div style={{display:"flex"}}>
-            <div className="chooseLocation">
+          <div className={styles.bookNowContainer} style={{ display: "flex", alignItems: "center" }}>
+            <div className={styles.chooseLocation}>
               <p>Location</p>
               <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                <InputLabel>Location Area</InputLabel>
-                <Select 
-                  value={selectedLocation}
-                  label="Location Area"
+                <Select
                   displayEmpty
+                  inputProps={{ "aria-label": "Without label" }}
+                  value={selectedLocation}
                   onChange={handleLocationChange}
                 >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
                   {locationAreasInSg.map((area, i) => {
-                    return <MenuItem key={i} value={area}>{area}</MenuItem>
+                    return (
+                      <MenuItem key={i} value={area}>
+                        {area}
+                      </MenuItem>
+                    );
                   })}
                 </Select>
               </FormControl>
             </div>
-            <div className="chooseDate">
+            <div className={styles.chooseDate}>
               <p>Date</p>
               <DatePicker
                 showIcon
@@ -115,18 +134,22 @@ function BookNow() {
                 highlightDates={listOfAvailDates}
                 dateFormat="dd/MM/YYYY"
                 filterDate={isSelectable}
+                isClearable
+                placeholderText="Choose a date!"
                 popperPlacement="top-end"
                 popperClassName={styles.popper}
+                wrapperClassName={styles.datePickerWrapper}
               />
             </div>
-            <button>Book Now </button>
+            <button className={styles.bookNowButton}>Book Now</button>
           </div>
+        </div>
+        <div className={styles.recyclingCircle}>
+          <img src={recyclingCircle} alt="circle" />
         </div>
       </div>
     </>
-
   );
-
 }
 
 export default BookNow;
