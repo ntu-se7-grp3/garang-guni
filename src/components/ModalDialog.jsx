32ea9a301c1bcf8cbaa335fd27d2b0eec761f847
Lolfoollor <L_Z_F47@hotmail.com>
2024-07-22 17:59:01 +0800

    Chore: Remove unnecessary props from ModalDialog

--- src/components/ModalDialog.jsx
+++ src/components/ModalDialog.jsx
@@ -1,6 +1,6 @@
 import { Dialog } from "@mui/material";
 
-function ModalDialog({ isOpen=false, handleClose=()=>{}, isDark = false, children}) {
+function ModalDialog({ isOpen=false, handleClose=()=>{}, children}) {
   return (
     <Dialog open={isOpen} onClose={handleClose}>
       {children}
